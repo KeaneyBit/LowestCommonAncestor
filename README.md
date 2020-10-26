462a1356c0be451a46560326ee70a22d370ce60f
John Keaney <trinity.keaney@gmail.com>
2020-10-26 15:47:31 +0000

    Finished LCA of DAG

--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 The java implementation of my LowestCommonAncestor function for my Software Engineering Module
 at Trinity College Dublin.
 
--- 16/10/2020
+**16/10/2020**
 Added basic Unit Testing to my implementation.
 
--- 17/10/2020
+**17/10/2020**
 Added Actual JUnit Java Class
 -- 01:33 Finished JUnit Tests
 
- -- 17/10/2020
+**17/10/2020**
  Began Directed Acyclic Graph Work
--- src/com/company/DAG_LowestCommonAncestor.java
+++ src/com/company/DAG_LowestCommonAncestor.java
@@ -4,91 +4,182 @@ package com.company;
 //so that I can compare my graph to a matrix
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class DAG_LowestCommonAncestor {
     //Node Class
-    class AdjListNode
-    {
+    class AdjListNode {
         private int v;
         private int weight;
-        AdjListNode(int _v, int _w) { v = _v;  weight = _w; }
-        int getV() { return v; }
-        int getWeight()  { return weight; }
+
+        AdjListNode(int _v, int _w) {
+            v = _v;
+            weight = _w;
+        }
+
+        int getV() {
+            return v;
+        }
+        int getWeight() {
+            return weight;
+        }
+        void setWeight(int newWeight) {
+            weight = newWeight;
+            return;
+        }
+
     }
 
     //DAG Graph
-    int V;
-    private LinkedList<AdjListNode>adj[];
+    private int E;
+    private int V;
+    private LinkedList<AdjListNode> adj[];
     private int[] visited;
 
-    DAG_LowestCommonAncestor(int v)
-    {
-        V=v;
+    DAG_LowestCommonAncestor(int v) {
+        V = v;
+        E = 0;
         adj = new LinkedList[V];
-        for (int i=0; i<v; ++i)
+        for (int i = 0; i < v; ++i) {
             adj[i] = new LinkedList<AdjListNode>();
+        }
         visited = new int[v];
+
     }
-    void addEdge(int u, int v, int weight)
-    {
-        AdjListNode node = new AdjListNode(v,weight);
-        adj[u].add(node);// Add v to u's list
-    }
-
-
-
-    public static void lowestCommonAncestor(){
+    void incE () {
+        E++;
         return;
     }
 
-    public boolean checkIfAcyclic() {
-        int count = 0;
-        for(int i =0;i<V;i++){
-            visited[count]=i;
-            for(int j = 0; j<V;j++){
-                for(int k=0;k<V;k++){
-                    if(visited[k]==j && adj[i].get(j).getV()==1){
-                        return true;
-                    }
+    public void addEdge(int u, int v) {
+        AdjListNode node = new AdjListNode(v, 1);
+        adj[u].add(node);// Add v to u's list
+        incE();
+    }
+
+
+    public static AdjListNode lowestCommonAncestor(DAG_LowestCommonAncestor g, int v, int w) {
+        if(g.checkIfAcyclic()) { return null; }
+        int[] vArr = new int[g.E];
+        int[] wArr = new int[g.E];
+        boolean[] vMarked = new boolean[g.V];
+        boolean[] wMarked = new boolean[g.V];
+        int vCount =0;
+        int wCount = 0;
+        vArr[vCount]=v;
+        wArr[wCount]=w;
+        for(int j=0; j<g.V;j++){//mark all vertices as not been visited yet
+            vMarked[j]=false;
+            wMarked[j]=false;
+        }
+        for(int i =0;i<g.V;i++){
+            vMarked[v] =true;
+            wMarked[w] =true;
+            for(int j = 0; j<g.adj[i].size();j++){
+                if(g.adj[i].get(j)!=null && vMarked[i]){
+                    vCount++;
+                    vArr[vCount]=j;
+                    vMarked[j]=true;
+                }
+                if(g.adj[i].get(j)!=null && wMarked[i]){
+                    wCount++;
+                    wArr[wCount]=j;
+                    wMarked[j]=true;
+                }
+                if(wArr[wCount]==vArr[vCount]){
+                    //return wArr[wCount];
+                    return g.adj[i].get(wArr[wCount]);
                 }
             }
-            count++;
         }
+        return null;//returns -1 if no ancestor found
+    }
+
+    private boolean isCyclic(int i, boolean[] visited, boolean[] recStack) {
+
+        // Mark the current node as visited and
+        // part of recursion stack
+        if (recStack[i])
+            return true;
+
+        if (visited[i])
+            return false;
+
+        visited[i] = true;
+
+        recStack[i] = true;
+        LinkedList<AdjListNode> children = adj[i];
+
+        for (AdjListNode c: children)
+            if (isCyclic(c.getV(), visited, recStack))
+                return true;
+
+        recStack[i] = false;
+
         return false;
+    }
+    boolean checkIfAcyclic () {
+        boolean[] visited = new boolean[V];
+        boolean[] recStack = new boolean[V];
+
+        for(int i = 0; i<V;i++)
+            if(isCyclic(i, visited, recStack))
+                return true;
+
+        return false;
+    }
+    public static void printGraph(DAG_LowestCommonAncestor cycle) {
+        for(int i =0; i < cycle.V; i++) {
+            if(cycle.adj[i].size() != 0) {
+                for(int j=0; j < cycle.adj[i].size(); j++) {
+                    System.out.print(i + "-> " + cycle.adj[i].get(j).getV() + " ");
+                }
+                System.out.println("");
+            }
+        }
+        System.out.println("");
     }
 
     public static void main(String[] args) {
 
-        DAG_LowestCommonAncestor g = new DAG_LowestCommonAncestor(6);
-        g.addEdge(0, 1, 5);
-        g.addEdge(0, 2, 3);
-        g.addEdge(0, 5, 3);
-        g.addEdge(1, 3, 6);
-        g.addEdge(1, 2, 2);
-        g.addEdge(2, 4, 4);
-        g.addEdge(2, 5, 2);
-        g.addEdge(2, 3, 7);
-        g.addEdge(3, 4, -1);
-        g.addEdge(4, 5, -2);
+        DAG_LowestCommonAncestor b = new DAG_LowestCommonAncestor(6);
+        b.addEdge(0, 1);
+        b.addEdge(0, 2);
+        if(b.checkIfAcyclic()) {System.out.println("hello there b");}
+        printGraph(b);
+        DAG_LowestCommonAncestor g = new DAG_LowestCommonAncestor(9);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 4);
+        g.addEdge(3, 5);
+        g.addEdge(4, 6);
+        g.addEdge(5, 7);
+        g.addEdge(6, 7);
+        g.addEdge(7, 8);
         //Print Graph
-        for(int i =0; i < g.V; i++) {
-            for(int j=0; j < g.adj[i].size(); j++) {
-                System.out.print(i + "-> " + g.adj[i].get(j).getV() + " ");
-            }
-            System.out.println("");
+        DAG_LowestCommonAncestor cycle = new DAG_LowestCommonAncestor(9);
+        cycle.addEdge(0, 1);
+        cycle.addEdge(0, 2);
+        cycle.addEdge(2, 1);
+        cycle.addEdge(1, 2);
+        cycle.addEdge(2, 4);
+        cycle.addEdge(4, 3);
+        cycle.addEdge(3, 1);
+        cycle.addEdge(3, 6);
+        cycle.addEdge(6, 8);
+        cycle.addEdge(7, 8);
+
+        printGraph(cycle);
+        printGraph(g);
+        if(cycle.checkIfAcyclic()) {
+            System.out.println("Mama Mia! There's a cycle!");
         }
         if(g.checkIfAcyclic()) {
-            System.out.print("Mama Mia! There no cycle!");
-        } else {
-            System.out.println("Graph is not Acyclic, please re-create");
+            System.out.println("Mama Mia! There's a cycle!");
         }
-
-
-
-        //
-        //0 -> 1 -> 3
-        //     !
-        //  -> 2 -> 4
-        //       -> 5
+        System.out.println(g.lowestCommonAncestor(g, 1, 5).getV()); //7
+        System.out.println(g.lowestCommonAncestor(g, 5, 2).getV()); //7
+        System.out.println(g.lowestCommonAncestor(g, 0, 1).getV()); //2
     }
 }
